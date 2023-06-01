6c4a3e630f7304a0f8c0e6bd4e8dde5733db8847
Kumsa Mergia <kumsamergia@gmail.com>
2023-06-01 11:54:20 +0300

    Update delete_department.php

--- admin/delete_department.php
+++ admin/delete_department.php
@@ -1,24 +1,21 @@
 
-
 <?php
- 
- require_once($_SERVER['DOCUMENT_ROOT'] .'/woliso-attendance/lang.php');
 
- require_once($_SERVER['DOCUMENT_ROOT'] .'/woliso-attendance/connection.php');
+require_once($_SERVER['DOCUMENT_ROOT'] .'/woliso-attendance/connection.php');
 
 if(isset($_POST['delete_btn'])) {
        $id = $_POST['delete_id'];
        
-       $query = "DELETE FROM department WHERE id='$id' ";
+       $query = "DELETE FROM employee WHERE id='$id' ";
        $query_run = mysqli_query($connection, $query);
 
        if ($query_run) {
-        $_SESSION['success'] = __('Department Data Deleted');
-        header ("Location: m_department.php");
+        $_SESSION['success'] ='Employee Data Delated';
+        header ("Location: ./m_employee.php");
     }
     else {
-        $_SESSION['success'] = __('Department Data NOT Deleted');
-        header ("Location: m_mepartment.php");
+        $_SESSION['success'] ='Employee Data NOT Delated';
+        header ("Location: ./m_employee.php");
     }
 }
 
