c129016fd6738cf7f9b8377655370aa01ab54779
Kumsa Mergia <kumsamergia@gmail.com>
2023-06-01 11:59:16 +0300

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WPTC | Employee Attendance System</title>
+    
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/about.css">
+	<script src = "assets/js/jquery-3.5.1.min.js"></script>
+	<script src = "assets/js/bootstrap.min.js"></script>
+    <script src ="assets/js/myscript.js"></script>
+</head>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 mr-auto ml-auto text-center py-5 mt-5">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="card-title bg-danger"> Not Available</h1>
+                <h2 class="card-title">404 Error</h2>
+                <p class="card-text">The Page you are searching for is Not Available.</p>
+            <a href="index.php" class="btn btn-primary">Go back to Home Page</a>
+            </div>
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+</body>
+</html>
